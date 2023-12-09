640c938d6b08e0cb5365fb6b339417b3dafb50c8
monisha <djmonisha.2022@gmail.com>
2023-12-09 21:16:07 +0530

    Designing AuthPage with Chakra-ui

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,19 +1,35 @@
-import { Box, Image, Input, VStack } from "@chakra-ui/react"
+import { Box,Button,Flex, Image, Input,Text, VStack } from "@chakra-ui/react";
+import { useState } from "react";
 
 const AuthForm = () => {
+    const [islogin, setIslogin] = useState(true);
+
   return (
     <>
     <Box border={"1px solid gray"} borderRadius={4} padding={5}>
-    <VStack spacing={4}>
-       <Image src='/logo.png' h={24} cursor={"pointer"} alt='Instagram' />
-       <Input/>
+        <VStack spacing={4}>
+            <Image src='/logo.png' h={24} cursor={"pointer"} alt='Instagram' />
+            <Input placeholder='Email' fontSize={14} type='email'/>
+            <Input placeholder='Password' fontSize={14} type='password' />
+
+            {!islogin ? <Input placeholder='Confirm Password' fontSize={14} type='password' /> : null}
+            <Button w={"full"} colorScheme='blue' size={"sm"} fontSize={14}>
+                {islogin ? "Log in" : "Sign Up"}
+
+            </Button>
+            {/* ------------------- OR ------------------ */}
+            <Flex alignItems={"center"} justifyContent={"center"} my={4} gap={1} w={"full"}>
+                <Box flex={2} h={"1px"} bg={"gray.400"}/>
+                <Text mx={1} color={"white"}>OR</Text>
+                <Box flex={2} h={"1px"} bg={"gray.400"}/>
+            </Flex>
 
         </VStack>
 
     </Box>
     
     </>
-  )
-}
+  );
+};
 
-export default AuthForm
+export default AuthForm;
